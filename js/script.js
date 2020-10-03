09559558fb5046b9b2cac6040ca939aced119870
Server1567 <chapijala@outlook.com>
2020-10-03 19:53:04 -0400

    comments

--- js/script.js
+++ js/script.js
@@ -11,23 +11,53 @@ $(document).ready(function(){
 
 		$.getJSON("https://jsonplaceholder.typicode.com/posts", function(dataProcessed) {
 			var dataForPage = dataProcessed.slice(beginning, end); // DATOS DESMENUZADOS :/
-			console.log(dataForPage);
+
 			// Ciclo que pone cada dato del array desmenuzado al DOM
 			for (var i = 0; i < dataForPage.length; i++) {
 				var title = dataForPage[i]['title'];
 				var post = dataForPage[i]['body'];
 				var id = dataForPage[i]['userId'];
+				var idPost = dataForPage[i]['id'];
 
-				$("#posts").append("<li class='post'><div class='post-view'><h4 class='post-title'></h4><br><h6 class='body-post'></h6><span class='comment-btn'>Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-down-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg></span></div></li>");
-
+				$("#posts").append("<li class='post'><div class='post-view'><h4 class='post-title'></h4><br><h6 class='body-post'></h6><span class='comment-btn' id='"+idPost+"'>Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-down-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg></span><div class='commentsBox switch'></div></div></li>");
 				$(".post-title").last().text(title);
 				$(".body-post").last().text(post);
 				$("#posts").attr("class", id);
 			}
+
+			$(".comment-btn").click(function () {
+				var postId = $(this).attr('id');
+				// .next() == .commentsBox
+				if ($(this).next().empty()) {
+					comments(postId);
+				}
+				else {
+					$(this).next().empty();
+				}
+				$(this).next().toggle().removeClass("switch");
+				// Aquí haremos la animación de entrada de los comentarios
+			});
 		});
 
  	}
  	posts(0, 10); // Ejecutamos los primeros Posts
+
+ 	function comments(post) {
+
+ 		var link = "https://jsonplaceholder.typicode.com/posts/"+post+"/comments";
+
+ 		$.getJSON(link, function(dataProcessed) {
+			// Ciclo que pone cada dato del array desmenuzado al DOM
+			for (var i = 0; i < dataProcessed.length; i++) {
+				var email = dataProcessed[i]['email'];
+				var name = dataProcessed[i]['name'];
+				var body = dataProcessed[i]['body'];
+
+				$("#"+post+"").next().append("<div class='comment'><h6 style='color: cadetblue;'>"+email+"</h6><strong>"+name+"</strong><br><p>"+body+"</p></div>");
+			}
+		});
+ 	}
+
  	
  	// PAGINATION
 
