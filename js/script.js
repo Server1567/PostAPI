caf2b98f3ccb4c3698f7f9c82ba3c32376a2e02e
Server1567 <chapijala@outlook.com>
2020-10-03 15:35:45 -0400

    pagination re-fixed

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@
 $(document).ready(function(){
 
  	function posts(beginning, end) {
-<<<<<<< HEAD
  		$("#posts").empty();
 
 		$.getJSON("https://jsonplaceholder.typicode.com/posts", function(dataProcessed) {
@@ -25,30 +24,9 @@ $(document).ready(function(){
 				$(".body-post").last().text(post);
 				$("#posts").attr("class", id);
 			}
-=======
-
-		fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(function(json) {
-			var data = json;
-			var dataForPage = data.slice(beginning, end); // DATOS DESMENUZADOS :/
-			console.log(dataForPage);
-			$.getJSON(dataForPage, function(dataProcessed) {
-
-				// Ciclo que pone cada dato del array desmenuzado al DOM
-				for (var i = 0; i < dataProcessed.length; i++) {
-					var title = data[i]['title'];
-					var post = data[i]['body'];
-
-					$("#posts").append("<li class='post'><div class='post-view'><h4 class='post-title'></h4><br><h6 class='body-post'></h6><span class='comment-btn'>Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-down-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg></span></div></li>");
-
-					$(".post-title").last().text(title);
-					$(".body-post").last().text(post);
-				}
-			});
->>>>>>> fead1f9656a9afc7018da0b9153669d0d356bf1e
 		});
 
  	}
-<<<<<<< HEAD
  	posts(0, 10); // Ejecutamos los primeros Posts
  	
  	// PAGINATION
@@ -244,8 +222,5 @@ $(document).ready(function(){
  			$("#next").addClass("disabled");
  		} 
  	});
-=======
- 	posts(0, 10);
->>>>>>> fead1f9656a9afc7018da0b9153669d0d356bf1e
 
 });
