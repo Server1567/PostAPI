697ee005f556cdfbae1a3ecb81227a66540ebb94
Server1567 <chapijala@outlook.com>
2020-10-03 23:15:34 -0400

    comments ready

--- js/script.js
+++ js/script.js
@@ -7,11 +7,10 @@
 $(document).ready(function(){
 
  	function posts(beginning, end) {
- 		$("#posts").empty();
 
 		$.getJSON("https://jsonplaceholder.typicode.com/posts", function(dataProcessed) {
 			var dataForPage = dataProcessed.slice(beginning, end); // DATOS DESMENUZADOS :/
-
+			$("#posts").empty();
 			// Ciclo que pone cada dato del array desmenuzado al DOM
 			for (var i = 0; i < dataForPage.length; i++) {
 				var title = dataForPage[i]['title'];
@@ -28,12 +27,12 @@ $(document).ready(function(){
 			$(".comment-btn").click(function () {
 				var postId = $(this).attr('id');
 				// .next() == .commentsBox
-				if ($(this).next().hasClass("switch")) {
+				if ($(this).next().hasClass("switch")) { // OPEN
 					comments(postId);
 					$(this).next().removeClass("switch");
 					$(this).html("Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-up-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/></svg>");
 				}
-				else {
+				else { // CLOSE
 					$(this).next().addClass("switch");
 					$(this).next().empty();
 					$(this).html("Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-down-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>");
@@ -56,7 +55,7 @@ $(document).ready(function(){
 				var name = dataProcessed[i]['name'];
 				var body = dataProcessed[i]['body'];
 
-				$("#"+post+"").next().append("<div class='comment'><h6 style='color: cadetblue;'>"+email+"</h6><strong>"+name+"</strong><br><p>"+body+"</p></div>");
+				$("#"+post+"").next().append("<div class='comment animate__animated animate__zoomIn'><h6 style='color: cadetblue;'>"+email+"</h6><strong>"+name+"</strong><br><p>"+body+"</p></div>");
 			}
 		});
  	}
