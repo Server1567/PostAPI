065add8f749aa65192884b40f3a359fc3c79c79e
Server1567 <chapijala@outlook.com>
2020-10-03 21:19:14 -0400

    logic finished

--- js/script.js
+++ js/script.js
@@ -28,13 +28,16 @@ $(document).ready(function(){
 			$(".comment-btn").click(function () {
 				var postId = $(this).attr('id');
 				// .next() == .commentsBox
-				if ($(this).next().empty()) {
+				if ($(this).next().hasClass("switch")) {
 					comments(postId);
+					$(this).next().removeClass("switch");
+					$(this).html("Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-up-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z'/></svg>");
 				}
 				else {
+					$(this).next().addClass("switch");
 					$(this).next().empty();
+					$(this).html("Comments<svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-caret-down-fill' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>");
 				}
-				$(this).next().toggle().removeClass("switch");
 				// Aquí haremos la animación de entrada de los comentarios
 			});
 		});
